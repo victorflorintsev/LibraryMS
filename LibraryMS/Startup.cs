03bd10a3b9a4fc37a68bf28cfb5ca4dad34d8969
Evelyn Galvan <igaulvhan@gmail.com>
2018-04-12 11:36:26 -0500

    Revert "Added Connection String for our database"
    This reverts commit 46b3eacac29f99150609e9897d082f1734c3db83.

--- LibraryMS/Startup.cs
+++ LibraryMS/Startup.cs
@@ -27,7 +27,7 @@ namespace LibraryMS
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("LibraryDatabase")));
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
