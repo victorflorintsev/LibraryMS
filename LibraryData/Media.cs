03de198eda58ec68b35129f2471c7cba71c6a6fe
psestepa <38301486+psestepa@users.noreply.github.com>
2018-04-14 18:24:13 -0500

    update
    Added Media Interface

---
+++ LibraryData/Media.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using LibraryData.LIBDBModels;
+using System.Text;
+
+namespace LibraryData
+{
+   public interface IMedia
+    {
+        //int id is the primary key
+        IEnumerable<Media> getAll(); 
+        Media getbyID(int id);
+        void Add(Media newMedia);
+        string getAuthorOrDirector(int id);
+        string getTitle(int id);
+        int getISBN(int id);
+        int getMediaType(int id);
+        string getCallNum(int id);
+        DateTime getDateAdded(int id);
+        int getCopiesLeft(int id);
+        int getMaxCopies(int id);
+
+
+
+    }
+}
---
+++ LibraryServices/MediaServices.cs
@@ -0,0 +1,80 @@
+﻿using LibraryData;
+using System;
+using LibraryData.LIBDBModels;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace LibraryServices
+{
+    public class MediaServices : IMedia
+    {
+        private LibraryMSContext _context;
+        public MediaServices(LibraryMSContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Media newMedia)
+        {
+            _context.Add(newMedia);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<Media> getAll()
+        {
+            return _context.Media;
+        }
+
+        public string getAuthorOrDirector(int id)
+        {
+            return _context.Media.FirstOrDefault(a => a.MediaId == id).Author;
+        }
+
+        public Media getbyID(int id)
+        {
+            return _context.Media.Include(asset=>asset.Title).Include(asset => asset.MediaType).FirstOrDefault(asset => asset.MediaId == id);   
+        }
+
+        public string getCallNum(int id)
+        {
+
+            return _context.Media.FirstOrDefault(a => a.MediaId == id).CallNum;
+        }
+
+        public int getCopiesLeft(int id)
+        {
+            return _context.Media.FirstOrDefault(a => a.MediaId == id).CopiesLeft;
+        }
+
+        public DateTime getDateAdded(int id)
+        {
+                return _context.Media.FirstOrDefault(a => a.MediaId == id).DateAdded;
+        }
+
+        public int getISBN(int id)
+        {
+            if (_context.Media.Any(a => a.MediaId == id))
+            {
+                return _context.Media.FirstOrDefault(a => a.MediaId == id).Isbn;
+            }
+            else
+                return 0;
+        }
+
+        public int getMaxCopies(int id)
+        {
+            return _context.Media.FirstOrDefault(a => a.MediaId == id).MaxCopies;
+        }
+
+        public int getMediaType(int id)
+        {
+            return _context.Media.FirstOrDefault(a => a.MediaId == id).MediaType;
+        }
+
+        public string getTitle(int id)
+        {
+            return _context.Media.FirstOrDefault(a => a.MediaId == id).Title;
+        }
+    }
+}
