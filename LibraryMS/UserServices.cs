c8beee9a0b2925d88d7e6012c441dd4df04248fc
Evelyn Galvan <igaulvhan@gmail.com>
2018-04-21 17:56:42 -0500

    Revert "Merge branch 'Branch-4-21' of https://github.com/victorflorintsev/LibraryMS into Branch-4-21"
    This reverts commit d97f6b32bdfaa98bac1910437987f1f7b3710b46, reversing
    changes made to 1464cb6a42d887e0f63161042aac440b79950203.

--- LibraryMS/UserServices.cs
+++ LibraryMS/UserServices.cs
@@ -21,17 +21,8 @@ namespace LibraryMS
 
         public List<Users> GetAll()
         {
-            /*List<Users> outList = new List<Users>();
-            IEnumerable<Users> a = _context.Users;
-            foreach (Users u in _context.Users()) {
-                outList.Add(u);
-            }*/
-            
-            //return outList;
             List<Users> outList = new List<Users>();
-            //outList.Add(_context.Users.First());
-            foreach (Users u in _context.Users)
-            {
+            foreach (Users u in _context.Users) {
                 outList.Add(u);
             }
             return outList;
